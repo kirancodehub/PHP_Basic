7a6802b882f7e111792ed5a5c1eea0ecf0f0a8d8
kiranavinash5 <kiranavinash1515@gmail.com>
2026-06-07 14:27:17 +0500

    addd file

---
+++ Calculator/23201_kiran_Calculator.php
@@ -0,0 +1,42 @@
+<?php
+
+echo "<html>";
+
+echo "<head>";
+
+echo "<title>Calculator</title>";
+
+echo "</head>";
+
+echo "<body>";
+
+echo "<h1>Calculator</h1>";
+
+$num1 = 40;
+$num2 =20;
+
+$addition = $num1 + $num2;
+
+echo "Addition of num1 and num2              = ". $addition;
+echo "<br/>";
+
+$subtraction = $num1 - $num2;
+
+echo "Subtraction of num1 and num2              = ". $subtraction;
+
+echo "<br/>";
+$multiplication = $num1 * $num2;
+
+echo "Multiplication of num1 and num2              = ". $multiplication;
+
+echo "<br/>";
+$devide = $num1 / $num2;
+
+echo "Devision of num1 and num2              = ". $devide;
+echo "<br/>";
+ 
+
+echo "</body>";
+
+echo "</html>";
+?>
